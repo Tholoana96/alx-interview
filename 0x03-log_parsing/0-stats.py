99a42a9dc7ec9ce0bea13fe5c2cbf2d7dad12c6a
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-08-13 22:18:05 +0200

    task complete

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def print_msg(dict_sc, total_file_size):
     """
     Method to print
@@ -11,39 +12,43 @@ def print_msg(dict_sc, total_file_size):
     Returns:
         Nothing
     """
+
     print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
         if val != 0:
             print("{}: {}".format(key, val))
 
+
 total_file_size = 0
+code = 0
 counter = 0
-dict_sc = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # Splitting the line into parts
-        if len(parsed_line) > 2:
-            # Accumulate the file size and status codes
-            counter += 1
-            total_file_size += int(parsed_line[-1])  # Assuming file size is the last element
-            code = parsed_line[-2]  # Assuming status code is the second last element
-            if code in dict_sc:
-                dict_sc[code] += 1
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-            if counter == 10:
+        if len(parsed_line) > 2:
+            counter += 1
+
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
                 print_msg(dict_sc, total_file_size)
                 counter = 0
 
-except KeyboardInterrupt:
+finally:
     print_msg(dict_sc, total_file_size)
-    raise
