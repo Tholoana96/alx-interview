b92e0588de90172cd17e388bae2104c700bf246c
Tholoana Manyane <tholoana.manyane@gmail.com>
2024-08-29 11:30:26 -0700

    trying my first solution

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,44 +2,43 @@
 """ N queens """
 import sys
 
-
 if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
 if not sys.argv[1].isdigit():
     print("N must be a number")
-    exit(1)
+    sys.exit(1)
 
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
-n = int(sys.argv[1])
+n_size = int(sys.argv[1])
 
-
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
+def queens(size, i=0, a=None, b=None, c=None):
+    """ Find possible positions """
+    if a is None: a = []
+    if b is None: b = []
+    if c is None: c = []
+    
+    if i < size:
+        for j in range(size):
             if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+                yield from queens(size, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
         yield a
 
+def solve(size):
+    """ Solve the N queens problem """
+    solution_set = []
+    row = 0
+    for solution in queens(size):
+        for col in solution:
+            solution_set.append([row, col])
+            row += 1
+        print(solution_set)
+        solution_set = []
+        row = 0
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
-
-
-solve(n)
-
+solve(n_size)
